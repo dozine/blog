2676203cc26bf65973a379fd8606ce772c97abcd
dozin <dojin0345@gmail.com>
2025-03-25 02:08:30 +0900

    feat : create category api

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -3,71 +3,43 @@ import styles from "./categoryList.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-const CategoryList = () => {
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+  return res.json();
+};
+
+const CategoryList = async () => {
+  const data = await getData();
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Categories</h1>
       <div className={styles.categories}>
-        <Link href={`/blog`} className={`${styles.category} ${styles.style}`}>
-          <Image
-            src="/style.png"
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          Programming
-        </Link>
-        <Link href={`/blog`} className={`${styles.category} ${styles.style}`}>
-          <Image
-            src="/style.png"
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          Design
-        </Link>
-        <Link href={`/blog`} className={`${styles.category} ${styles.style}`}>
-          <Image
-            src="/style.png"
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          Travel
-        </Link>
-        <Link href={`/blog`} className={`${styles.category} ${styles.style}`}>
-          <Image
-            src="/style.png"
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          Architect
-        </Link>
-        <Link href={`/blog`} className={`${styles.category} ${styles.style}`}>
-          <Image
-            src="/style.png"
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          Novel
-        </Link>
-        <Link href={`/blog`} className={`${styles.category} ${styles.style}`}>
-          <Image
-            src="/style.png"
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          Movie
-        </Link>
+        {data?.map((item) => {
+          return (
+            <Link
+              href="/blog?cat=style"
+              className={`${styles.category} ${styles[item.slug]}`}
+              key={item._id}
+            >
+              {item.img && (
+                <Image
+                  src={item.img}
+                  alt=""
+                  width={32}
+                  height={32}
+                  className={styles.image}
+                />
+              )}
+              {item.title}
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
