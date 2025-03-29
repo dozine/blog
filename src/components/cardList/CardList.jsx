909e7ab67642e6b6dc387aa6a52144158dde09eb
dozin <dojin0345@gmail.com>
2025-03-29 16:28:00 +0900

    feat : create blog page api

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,20 +1,60 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import styles from "./cardList.module.css";
 import Pagination from "../pagination/Pagination";
-import Image from "next/image";
 import Card from "../card/Card";
+import { useSearchParams } from "next/navigation";
+
+const POSTS_PER_PAGE = 2;
 
 const CardList = () => {
+  const searchParams = useSearchParams();
+  const page = Number(searchParams.get("page")) || 1;
+  const cat = searchParams.get("cat") || "";
+
+  const [posts, setPosts] = useState([]);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await fetch(
+          `http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,
+          {
+            cache: "no-store",
+          }
+        );
+        if (!res.ok) {
+          throw new Error("Failed");
+        }
+        const data = await res.json();
+        setPosts(data.posts);
+        setCount(data.count);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getData();
+  }, [page, cat]);
+
+  const hasPrev = page > 1;
+  const hasNext = POSTS_PER_PAGE * (page - 1) + POSTS_PER_PAGE < count;
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
       <div className={styles.posts}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {posts.length > 0 ? (
+          posts.map((item) => (
+            <div key={item.id}>
+              <Card item={item} />
+            </div>
+          ))
+        ) : (
+          <p>POST가 없습니다.</p>
+        )}
       </div>
-      <Pagination />
+      <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext} />
     </div>
   );
 };
