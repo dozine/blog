438b51c6ab79c6b7e87e7a6e6414c25ace37ae38
dozin <dojin0345@gmail.com>
2025-03-30 02:41:45 +0900

    feat :remove social button and menu

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,24 @@
+"use client";
+
 import CategoryList from "@/components/categoryList/CategoryList";
 import styles from "./homepage.module.css";
 import Featured from "@/components/featured/Featured";
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/menu/Menu";
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
+  const searchParams = useSearchParams();
+  const pageParam = searchParams.get("page");
+  const [page, setPage] = useState(pageParam ? parseInt(pageParam, 10) : 1);
+
   return (
     <div className={styles.container}>
       <Featured />
       <CategoryList />
       <div className={styles.content}>
-        <CardList />
-        <Menu />
+        <CardList page={page} />
       </div>
     </div>
   );
