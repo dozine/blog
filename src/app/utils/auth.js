61678ae995d716946a75f5212aa3d3f6c96731fc
dozin <dojin0345@gmail.com>
2025-04-23 19:14:55 +0900

    refactor: admin email만 로그인 허용

--- src/app/utils/auth.js
+++ src/app/utils/auth.js
@@ -4,6 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 import prisma from "./connect";
 import { getServerSession } from "next-auth";
 
+const ADMIN_EMAIL = process.env.MYEMAIL;
+
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
   // javascript와 typescript연결 방식이 다름 .
@@ -17,6 +19,22 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      if (user?.email === ADMIN_EMAIL) {
+        return true; // 로그인 허용
+      } else {
+        return false; // 로그인 거부
+      }
+    },
+    async session({ session }) {
+      if (session?.user?.email === ADMIN_EMAIL) {
+        return session;
+      } else {
+        return null; // 세션 제거
+      }
+    },
+  },
 };
 
 export const getAuthSession = () => getServerSession(authOptions);
