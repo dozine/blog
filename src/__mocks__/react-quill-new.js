5840d1308718bf513511ca04e6b1218c2c74af1b
dozin <dojin0345@gmail.com>
2025-05-28 04:51:04 +0900

    feat: Writepage 단위테스트 구현

---
+++ src/__mocks__/react-quill-new.js
@@ -0,0 +1,21 @@
+// __mocks__/react-quill-new.js
+import React from "react";
+
+const MockReactQuill = ({ value, onChange, ...props }) => (
+  <div data-testid="mock-react-quill">
+    <textarea
+      data-testid="quill-textarea"
+      value={value || ""}
+      onChange={(e) => onChange && onChange(e.target.value)}
+      placeholder="Write your story..."
+      {...props}
+    />
+  </div>
+);
+
+const Quill = {
+  register: jest.fn(),
+};
+
+export default MockReactQuill;
+export { Quill };
