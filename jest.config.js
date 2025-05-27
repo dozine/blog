a9116221102d8dee9527887883191cd7f16ce76f
dozin <dojin0345@gmail.com>
2025-05-28 04:47:27 +0900

    chore : filemock 추가

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     "^@/(.*)$": "<rootDir>/src/$1",
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/src/mocks/fileMock.js",
+      "<rootDir>/src/__mocks__/fileMock.js",
   },
   transform: {
     "^.+\\.(js|jsx)$": ["babel-jest", { configFile: "./babel-jest.config.js" }],
