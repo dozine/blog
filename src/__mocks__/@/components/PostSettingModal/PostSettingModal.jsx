b71793bb3303489f4093f587fd45f9a6eb8b250f
dozin <dojin0345@gmail.com>
2025-05-28 04:51:59 +0900

    feat: Writepage 테스트용 내부 컴포넌트

---
+++ src/__mocks__/@/components/PostSettingModal/PostSettingModal.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { jest } from "@jest/globals";
+const MockPostSettingModal = jest.fn((props) => {
+  if (!props.isOpen) return null;
+  return (
+    <div data-testid="mock-post-setting-modal">
+      <button data-testid="mock-modal-close-button" onClick={props.onClose}>
+        모달 닫기
+      </button>
+      <button data-testid="mock-modal-publish-button" onClick={props.onPublish}>
+        모달에서 게시
+      </button>
+    </div>
+  );
+});
+
+export default MockPostSettingModal;
