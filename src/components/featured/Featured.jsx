f5fe3ab72adf2c1893ad4ed76ae68a8bd5738998
dozin <dojin0345@gmail.com>
2025-06-07 00:31:42 +0900

    fix : featured index추가

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -31,10 +31,12 @@ const Featured = () => {
   };
   return (
     <div className={styles.container}>
-      <div className={styles.featuredTitle}>안녕하세요. 블로그 진도장입니다.</div>
+      <div className={styles.featuredTitle}>
+        안녕하세요. 블로그 진도장입니다.
+      </div>
 
       <Slider {...settings}>
-        {featuredPosts.map((post) => (
+        {featuredPosts.map((post, index) => (
           <div key={post.id} className={styles.slide}>
             <div className={styles.imgContainer}>
               <Image
