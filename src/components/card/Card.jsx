959d2006b4759ef38ed1effbdf2e8a6953248896
dozin <dojin0345@gmail.com>
2025-06-06 23:58:30 +0900

    fix : lint 적용

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -8,9 +8,7 @@ const Card = ({ item, priority = false }) => {
       <div className={styles.container}>
         <div className={styles.textContainer}>
           <div className={styles.detail}>
-            <span className={styles.date}>
-              {item.createdAt.substring(0, 10)} -{" "}
-            </span>
+            <span className={styles.date}>{item.createdAt.substring(0, 10)} - </span>
             <span className={styles.category}>{item.catSlug}</span>
           </div>
           <div className={styles.titleContainer}>
@@ -19,9 +17,7 @@ const Card = ({ item, priority = false }) => {
           <p>
             {(() => {
               const descText = item.desc.replace(/<[^>]+>/g, "");
-              return descText.length > 60
-                ? descText.substring(0, 60) + "..."
-                : descText;
+              return descText.length > 60 ? descText.substring(0, 60) + "..." : descText;
             })()}
           </p>
           {console.log("Card received tags:", item.tags)}
@@ -35,22 +31,20 @@ const Card = ({ item, priority = false }) => {
             </div>
           )}
         </div>
-        {Array.isArray(item.img) &&
-          item.img.length > 0 &&
-          item.img[0].trim() !== "" && (
-            <div className={styles.imageContainer}>
-              <div className={styles.image}>
-                <Image
-                  src={item.img[0]}
-                  alt={item.title || "포스트 이미지"}
-                  fill
-                  style={{ objectFit: "cover" }}
-                  priority={priority}
-                  sizes="(max-width: 767px) 100vw, 280px"
-                />
-              </div>
+        {Array.isArray(item.img) && item.img.length > 0 && item.img[0].trim() !== "" && (
+          <div className={styles.imageContainer}>
+            <div className={styles.image}>
+              <Image
+                src={item.img[0]}
+                alt={item.title || "포스트 이미지"}
+                fill
+                style={{ objectFit: "cover" }}
+                priority={priority}
+                sizes="(max-width: 767px) 100vw, 280px"
+              />
             </div>
-          )}
+          </div>
+        )}
       </div>
     </Link>
   );
